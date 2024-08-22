36dc563dc77c83c7ed45e7cb181b2ed38553ada2
qr3n <qren.freelance@gmail.com>
2024-08-22 05:31:49 -0700

    Init

--- src/pages/Home/ui/Page/Page.tsx
+++ src/pages/Home/ui/Page/Page.tsx
@@ -13,9 +13,13 @@ const Home: FC = () => {
               className="max-w-sm rounded-lg shadow-2xl"
             />
             <div>
-              <p className="text-5xl font-bold"></p>
-              <p className="py-5 flex-wrap">
-                { initData === undefined ? 'undefined' : initData }
+              <h1 className="text-5xl font-bold">{ initData === undefined ? 'undefined' : initData }</h1>
+              <p className="py-5">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
+                minima laboriosam maxime sed dignissimos harum provident itaque
+                fugiat. A repellat aliquid inventore dolor tempora, omnis
+                perferendis aspernatur quo nisi excepturi. Ex, ullam odio iusto
+                esse necessitatibus doloremque repudiandae!
               </p>
               <button className="btn-primary btn">Get Started</button>
             </div>
