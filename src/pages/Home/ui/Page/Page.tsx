157b5e33e3280dbfce725d153f1ea1dee32e2318
qr3n <qren.freelance@gmail.com>
2024-08-22 05:34:21 -0700

    Init

--- src/pages/Home/ui/Page/Page.tsx
+++ src/pages/Home/ui/Page/Page.tsx
@@ -2,34 +2,13 @@ import {FC} from "react";
 import {useInitData} from "@vkruglikov/react-telegram-web-app";
 
 const Home: FC = () => {
-  const [f, initData] = useInitData()
+  const [initData] = useInitData()
 
-  console.log(f)
   console.log(initData)
 
   return (
     <>
-      <section>
-        <div className="hero min-h-[calc(100vh-64px)] bg-base-200">
-          <div className="hero-content flex-col lg:flex-row">
-            <img
-              src="/images/hero.webp"
-              className="max-w-sm rounded-lg shadow-2xl"
-            />
-            <div>
-              <h1 className="text-5xl font-bold">{ initData === undefined ? 'undefined' : initData }</h1>
-              <p className="py-5">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
-                minima laboriosam maxime sed dignissimos harum provident itaque
-                fugiat. A repellat aliquid inventore dolor tempora, omnis
-                perferendis aspernatur quo nisi excepturi. Ex, ullam odio iusto
-                esse necessitatibus doloremque repudiandae!
-              </p>
-              <button className="btn-primary btn">Get Started</button>
-            </div>
-          </div>
-        </div>
-      </section>
+
     </>
   );
 };
