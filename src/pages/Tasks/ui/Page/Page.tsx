e7634d036ae5a1ff2d89b839fa5239545b99fe2f
qr3n <qren.freelance@gmail.com>
2024-08-22 09:13:15 -0700

    Init

--- src/pages/Tasks/ui/Page/Page.tsx
+++ src/pages/Tasks/ui/Page/Page.tsx
@@ -1,20 +1,27 @@
 import { FC } from "react";
 import bell from './bell.webp'
 import earn from './earn.png'
+import {Link} from "react-router-dom";
 
 const Tasks: FC = () => {
   return (
       <div
           className='w-full px-8 flex items-center flex-col justify-center absolute z-50 top-[25%] -translate-y-10 left-1/2 -translate-x-1/2'>
           <h1 className='text-black text-6xl font-bold'>Tasks</h1>
-          <div className='p-4 bg-white w-full rounded-2xl mt-6 text-[#303131] flex gap-3 items-center font-semibold'>
-              <img src={bell} alt='' className='w-8'/>
-              Subscribe
-          </div>
-          <div className='p-4 bg-white w-full rounded-2xl mt-3 text-[#303131] flex gap-3 items-center font-semibold'>
-              <img src={earn} alt='' className='w-8'/>
-              Earn
-          </div>
+          <Link to='/tasks/subscribe' className='w-full'>
+              <div
+                  className='p-4 bg-white w-full rounded-2xl mt-6 text-[#303131] flex gap-3 items-center font-semibold'>
+                  <img src={bell} alt='' className='w-8'/>
+                  Subscribe
+              </div>
+          </Link>
+          <Link to='/tasks/earn' className='w-full'>
+              <div
+                  className='p-4 bg-white w-full rounded-2xl mt-3 text-[#303131] flex gap-3 items-center font-semibold'>
+                  <img src={earn} alt='' className='w-8'/>
+                  Earn
+              </div>
+          </Link>
       </div>
   );
 };
