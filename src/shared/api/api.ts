e6e9ba87dfc0d9f49b0ab60760a32c2cd0acc858
qr3n <qren.freelance@gmail.com>
2024-08-26 04:12:41 -0700

    Init

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'https://deploy-python-fastapi-in-vercel-qr3n-qr3ns-projects.vercel.app',
+    baseURL: 'https://dog-telegram.vercel.app',
 })
